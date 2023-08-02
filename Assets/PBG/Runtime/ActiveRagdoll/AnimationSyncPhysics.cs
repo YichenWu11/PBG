5e798dbea2c52026e6349526fb404a7615c6e2af
yichenwu11 <yichenwu11@outlook.com>
2023-08-02 20:44:30 +0800

    8/2

---
+++ Assets/PBG/Runtime/ActiveRagdoll/AnimationSyncPhysics.cs
@@ -0,0 +1,135 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using PBG.Runtime;
+using UnityEngine;
+
+namespace PBG.Runtime
+{
+    public class AnimationSyncPhysics : MonoBehaviour
+    {
+        private Animator m_Animator;
+        [SerializeField] private ActiveRagdoll m_ActiveRagdoll;
+        [SerializeField] private bool m_SyncPhysicsBody = true;
+        [SerializeField] private bool m_IKEnabled = true;
+
+        private ThirdPersonCamera m_ThirdPersonCamera;
+        private IKControl m_IKControl;
+
+        private Transform m_AnimatedTorso;
+        private Transform m_Chest;
+
+        public float MinTargetDirAngle = -30;
+        public float MaxTargetDirAngle = 60;
+
+        public float MinLookAngle = -50;
+        public float MaxLookAngle = 60;
+
+        public float MinArmAngle = -70;
+        public float MaxArmAngle = 100;
+
+        public float ArmsHorizontalSeparation = 0.75f;
+
+        public Vector3 AimedDirection
+        {
+            set
+            {
+                m_TargetDirection = value;
+                m_AimedDirection = Quaternion.LookRotation(value, Vector3.up);
+            }
+        }
+
+        private Vector3 m_TargetDirection;
+        private Quaternion m_AimedDirection;
+        private float m_TargetDirVerticalPercent;
+
+        private void OnValidate()
+        {
+            if (m_ActiveRagdoll == null) m_ActiveRagdoll = GetComponent<ActiveRagdoll>();
+        }
+
+        private void Awake()
+        {
+            m_IKControl = m_ActiveRagdoll.AnimatedAnimator.gameObject.AddComponent<IKControl>();
+        }
+
+        private void Start()
+        {
+            m_Animator = m_ActiveRagdoll.AnimatedAnimator;
+            m_ThirdPersonCamera = GetComponent<ThirdPersonCamera>();
+            m_AnimatedTorso = m_ActiveRagdoll.AnimatedTorso;
+            m_Chest = m_Animator.GetBoneTransform(HumanBodyBones.Spine);
+        }
+
+        private void Update()
+        {
+            SyncWithPhysics();
+        }
+
+        private void FixedUpdate()
+        {
+            UpdateIK();
+        }
+
+        private void SyncWithPhysics()
+        {
+            if (!m_SyncPhysicsBody) return;
+            m_ActiveRagdoll.AnimatedAnimator.transform.position =
+                m_ActiveRagdoll.PhysicalTorso.position +
+                (m_ActiveRagdoll.AnimatedAnimator.transform.position -
+                 m_AnimatedTorso.position);
+            m_ActiveRagdoll.AnimatedAnimator.transform.rotation = m_ActiveRagdoll.PhysicalTorso.rotation;
+        }
+
+        private void UpdateIK()
+        {
+            if (!m_IKEnabled)
+            {
+                m_IKControl.LookIKWeight = 0;
+                m_IKControl.LeftArmIKWeight = 0;
+                m_IKControl.RightArmIKWeight = 0;
+                return;
+            }
+
+            // UpdateIKLookPoint
+            var camAimedDir = m_ThirdPersonCamera.Camera.transform.forward;
+            var lookingBackwards = Vector3.Angle(camAimedDir, m_AnimatedTorso.forward) > 90;
+            if (lookingBackwards)
+                camAimedDir = Vector3.Reflect(camAimedDir, m_AnimatedTorso.forward);
+            var camAimedDir2D = Vector3.ProjectOnPlane(camAimedDir, Vector3.up).normalized;
+
+            var directionAngle = Vector3.Angle(camAimedDir, Vector3.up) - 90f;
+            m_TargetDirVerticalPercent = 1 - Mathf.Clamp01((directionAngle - MinTargetDirAngle) /
+                                                           Mathf.Abs(MaxTargetDirAngle - MinTargetDirAngle));
+
+            var lookVerticalAngle = m_TargetDirVerticalPercent * Mathf.Abs(MaxLookAngle - MinLookAngle) + MinLookAngle;
+            var lookDir = Quaternion.AngleAxis(-lookVerticalAngle, m_AnimatedTorso.right) * camAimedDir2D;
+
+            m_IKControl.LookPoint = m_Animator.GetBoneTransform(HumanBodyBones.Head).position + lookDir;
+
+            // Update Arm Targets
+            var armsVerticalAngle = m_TargetDirVerticalPercent * Mathf.Abs(MaxArmAngle - MinArmAngle) + MinArmAngle;
+            var armsDir = Quaternion.AngleAxis(-armsVerticalAngle, m_AnimatedTorso.right) * camAimedDir2D;
+            var currentArmsDistance = m_TargetDirVerticalPercent / 2 + 0.5f;
+
+            var armsMiddleTarget = m_Chest.position + armsDir * currentArmsDistance;
+            var upRef = Vector3.Cross(armsDir, m_AnimatedTorso.right).normalized;
+            var armsHorizontalVec = Vector3.Cross(armsDir, upRef).normalized;
+
+            m_IKControl.LeftHandIKTarget.position =
+                armsMiddleTarget + armsHorizontalVec * ArmsHorizontalSeparation / 2;
+            m_IKControl.RightHandIKTarget.position =
+                armsMiddleTarget - armsHorizontalVec * ArmsHorizontalSeparation / 2;
+        }
+
+        public void LeftArmProcess(float value)
+        {
+            m_IKControl.LeftArmIKWeight = value;
+        }
+
+        public void RightArmProcess(float value)
+        {
+            m_IKControl.RightArmIKWeight = value;
+        }
+    }
+}
---
+++ Assets/PBG/Runtime/ActiveRagdoll/PhysicsSyncAnimation.cs
@@ -0,0 +1,154 @@
+using System;
+using UnityEngine;
+using PBG.Runtime.Util;
+
+namespace PBG.Runtime
+{
+    public class PhysicsSyncAnimation : MonoBehaviour
+    {
+        [SerializeField] private ActiveRagdoll m_ActiveRagdoll;
+
+        private ConfigurableJoint[] m_Joints;
+        private Transform[] m_AnimatedBones;
+        private Quaternion[] m_IniJointsRotations;
+
+        [SerializeField] private float m_RotationSpeed = 2f;
+
+        [SerializeField] [Range(0, 60)] private float m_MaxSlopeAngle = 45f;
+
+        [SerializeField] private GrabControl m_GrabControl;
+
+        public bool IsGrabbing => m_GrabControl && m_GrabControl.IsGrabbing;
+
+        public bool WantToJump { get; set; } = false;
+        public bool IsJumping { get; set; } = false;
+        public bool IsOnGround { get; set; } = true;
+
+        public float SpeedUpRatio { get; set; } = 0;
+
+        [SerializeField] private float m_JumpImpluse = 300f;
+        [SerializeField] private float m_ShakeImpluse = 30f;
+        [SerializeField] private float m_ShakeDir = 1;
+
+        // public float MaxSpeedMulti = 2.5f;
+        // public float MinSpeedMulti = 1.5f;
+
+
+        public Vector3 AimedDirection
+        {
+            set
+            {
+                m_TargetDirection = value;
+                m_AimedDirection = Quaternion.LookRotation(value, Vector3.up);
+            }
+        }
+
+        private Vector3 m_TargetDirection;
+        private Quaternion m_AimedDirection;
+
+        private void OnValidate()
+        {
+            if (m_ActiveRagdoll == null) m_ActiveRagdoll = GetComponent<ActiveRagdoll>();
+            if (m_GrabControl == null) m_GrabControl = GetComponent<GrabControl>();
+        }
+
+        private void Start()
+        {
+            m_Joints = m_ActiveRagdoll.Joints;
+            m_AnimatedBones = m_ActiveRagdoll.AnimatedBones;
+
+            // 缓存关节初始旋转
+            m_IniJointsRotations = new Quaternion[m_Joints.Length];
+            for (var i = 0; i < m_Joints.Length; ++i)
+                m_IniJointsRotations[i] = m_Joints[i].transform.localRotation;
+
+            // 冻结躯干旋转
+            m_ActiveRagdoll.PhysicalTorso.constraints = RigidbodyConstraints.FreezeRotation;
+        }
+
+        private void FixedUpdate()
+        {
+            if (IsOnGround && WantToJump)
+            {
+                m_ActiveRagdoll.PhysicalTorso.AddForce(Vector3.up * m_JumpImpluse, ForceMode.Impulse);
+                WantToJump = false;
+            }
+
+            if (!IsOnGround && IsGrabbing)
+                m_ActiveRagdoll.PhysicalTorso.AddForce(m_ShakeImpluse * m_ShakeDir * m_TargetDirection,
+                    ForceMode.Force);
+
+            KeepBalance();
+            SyncWithAnimation();
+
+            // SpeedingUp By SpeedUpRatio
+            // var curVel = SpeedUpRatio < -0.5f
+            //     ? Vector3.zero
+            //     : (SpeedUpRatio * (MaxSpeedMulti - MinSpeedMulti) + MinSpeedMulti) * m_TargetDirection;
+            // m_ActiveRagdoll.PhysicalTorso.velocity = curVel;
+            // m_ActiveRagdoll.PhysicalAnimator.GetBoneTransform(HumanBodyBones.LeftFoot).GetComponent<Rigidbody>()
+            //     .velocity = curVel;
+            // m_ActiveRagdoll.PhysicalAnimator.GetBoneTransform(HumanBodyBones.RightFoot).GetComponent<Rigidbody>()
+            //     .velocity = curVel;
+
+            var forceDir = CalculateSlopeDirection();
+            if (forceDir != Vector3.zero && Vector3.Dot(m_TargetDirection, forceDir) > -0.5f)
+            {
+                // 从 MinAngle 到 MaxAngle 插值
+                var forceMagnitude = Mathf.Lerp(4f, 12f, Vector3.Angle(forceDir, Vector3.forward));
+                m_ActiveRagdoll.PhysicalTorso.AddForce(forceDir * forceMagnitude, ForceMode.Impulse);
+            }
+        }
+
+        private void SyncWithAnimation()
+        {
+            for (var i = 0; i < m_Joints.Length; i++)
+                // (i + 1) => skip the first bone (Torso)
+                m_Joints[i].SetTargetRotationLocal(
+                    m_AnimatedBones[i + 1].localRotation,
+                    m_IniJointsRotations[i]);
+        }
+
+        private void KeepBalance()
+        {
+            // 转向到 AimedDirection
+            var rotation =
+                Quaternion.Lerp(
+                    m_ActiveRagdoll.PhysicalTorso.rotation,
+                    m_AimedDirection,
+                    Time.fixedDeltaTime * m_RotationSpeed);
+            m_ActiveRagdoll.PhysicalTorso.MoveRotation(rotation);
+        }
+
+        public void JumpProcess(bool value)
+        {
+            WantToJump = value;
+            IsJumping = value;
+        }
+
+        public void ShakeProcess(Vector2 value)
+        {
+            if (IsGrabbing) m_ShakeDir = value.y;
+        }
+
+        private Vector3 CalculateSlopeDirection()
+        {
+            if (Physics.Raycast(
+                    m_ActiveRagdoll.PhysicalTorso.position,
+                    Vector3.down,
+                    out var hitInfo, 0.8f))
+            {
+                var slopeAngle = Vector3.Angle(hitInfo.normal, Vector3.up);
+                if (slopeAngle > 0 && slopeAngle <= m_MaxSlopeAngle)
+                {
+                    // 计算斜坡的方向
+                    var rightDirection = Vector3.Cross(hitInfo.normal, Vector3.up);
+                    var slopeDirection = Vector3.Cross(rightDirection, hitInfo.normal);
+                    return slopeDirection;
+                }
+            }
+
+            return Vector3.zero;
+        }
+    }
+}
